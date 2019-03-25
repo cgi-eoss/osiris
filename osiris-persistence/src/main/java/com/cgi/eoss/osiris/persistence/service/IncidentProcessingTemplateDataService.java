487674881e7c2afa47a7cf6f6b7f73f02138f0bb
Antonio Cuomo <antonio.cuomo@cgi.com>
2019-03-25 10:54:08 +0100

    Fix class of incident processing template data service
    Change-Id: I9177a5a6637ddfe1ba72a22c0613465ceee622e9

--- osiris-persistence/src/main/java/com/cgi/eoss/osiris/persistence/service/IncidentProcessingTemplateDataService.java
+++ osiris-persistence/src/main/java/com/cgi/eoss/osiris/persistence/service/IncidentProcessingTemplateDataService.java
@@ -1,12 +1,11 @@
 package com.cgi.eoss.osiris.persistence.service;
 
-import com.cgi.eoss.osiris.model.Incident;
 import com.cgi.eoss.osiris.model.IncidentProcessingTemplate;
 import com.cgi.eoss.osiris.model.User;
 
 import java.util.List;
 
-public interface IncidentProcessingTemplateDataService extends OsirisEntityDataService<Incident> {
+public interface IncidentProcessingTemplateDataService extends OsirisEntityDataService<IncidentProcessingTemplate> {
 
     List<IncidentProcessingTemplate> findByOwner(User user);
 
