72b7f4e1f83b7b042cd333651a2240b71b33e96b
Antonio Cuomo <antonio.cuomo@cgi.com>
2019-06-28 18:56:51 +0200

    Fix another psql migration typo
    Change-Id: I70f2cd63877e8cf9d19e66416ae319e0e51bd279

--- osiris-persistence/src/main/resources/db/migration/postgresql/V21__Incidents_collections_associations.sql
+++ osiris-persistence/src/main/resources/db/migration/postgresql/V21__Incidents_collections_associations.sql
@@ -1,7 +1,7 @@
  -- Incidents to collections
 CREATE TABLE osiris_incidents_collections (
   incident_id BIGINT NOT NULL REFERENCES osiris_incidents (id),
-  collection_id       BIGINT NOT NULL REFERENCES osiris_collections (id)
+  collection_id       BIGINT NOT NULL REFERENCES osiris_collections (id),
   PRIMARY KEY (incident_id, collection_id)
 );
 
