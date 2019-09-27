44817b137d0e7ebd8d2b5a3d27a26dc0b0af451e
Antonio Cuomo <antonio.cuomo@cgi.com>
2019-09-27 10:14:19 +0200

    Customise job termination based on service type
    Change-Id: I7d4dd20d742aba8aae3cf2efbc2c37f910d0cff8

--- osiris-api/src/main/java/com/cgi/eoss/osiris/api/mappings/JobResourceProcessor.java
+++ osiris-api/src/main/java/com/cgi/eoss/osiris/api/mappings/JobResourceProcessor.java
@@ -8,10 +8,13 @@ import org.springframework.hateoas.Resource;
 import org.springframework.hateoas.ResourceProcessor;
 import org.springframework.stereotype.Component;
 import com.cgi.eoss.osiris.model.OsirisFile;
+import com.cgi.eoss.osiris.model.OsirisService;
 import com.cgi.eoss.osiris.model.Job;
+import com.cgi.eoss.osiris.model.Job.Status;
 import com.cgi.eoss.osiris.model.JobStep;
 import com.cgi.eoss.osiris.model.projections.DetailedJob;
 import com.cgi.eoss.osiris.model.projections.ShortJob;
+import com.cgi.eoss.osiris.persistence.service.JobDataService;
 import com.cgi.eoss.osiris.persistence.service.OsirisFileDataService;
 import com.google.common.base.Strings;
 import com.google.common.collect.Multimap;
@@ -27,6 +30,7 @@ public class JobResourceProcessor extends BaseResourceProcessor<Job> {
 
     private final RepositoryEntityLinks entityLinks;
     private final OsirisFileDataService osirisFileDataService;
+    private final JobDataService jobDataService;
 
     @Override
     protected EntityLinks getEntityLinks() {
@@ -61,12 +65,22 @@ public class JobResourceProcessor extends BaseResourceProcessor<Job> {
         }
     }
 
-    private void addTerminateLink(Resource resource, Job.Status status, String stage) {
-        if (JobStep.PROCESSING.getText().equals(stage)) {
-            // TODO Do this properly with a method reference
-            resource.add(new Link(resource.getLink("self").getHref() + "/terminate").withRel("terminate"));
-        }
-    }
+    private void addTerminateLink(Resource resource, Long jobId) {
+        Job job = jobDataService.getById(jobId);
+        addTerminateLink(resource, job.getStatus(), job.getStage(), job.getConfig().getService().getType());
+     }
+     
+     private void addTerminateLink(Resource resource, Status status, String stage, OsirisService.Type serviceType) {
+     	//FTP Service can be stopped during the processing stage, even if in error
+     	if (serviceType.equals(OsirisService.Type.FTP_SERVICE) && JobStep.PROCESSING.getText().equals(stage)) {
+     		resource.add(new Link(resource.getLink("self").getHref() + "/terminate").withRel("terminate"));
+     	}
+     	//Other services can be stopped while their status is running
+     	if (!serviceType.equals(OsirisService.Type.FTP_SERVICE) && status == Job.Status.RUNNING && JobStep.PROCESSING.getText().equals(stage)) {
+     	    resource.add(new Link(resource.getLink("self").getHref() + "/terminate").withRel("terminate"));
+     	}
+
+     }
     
     private void addCancelLink(Resource resource, Job.Status status) {
         if (status == Job.Status.CREATED) {
@@ -85,7 +99,7 @@ public class JobResourceProcessor extends BaseResourceProcessor<Job> {
             addGuiLink(resource, entity.getStatus(), entity.getGuiUrl());
             addLogsLink(resource);
             addOutputLinks(resource, entity.getOutputs());
-            addTerminateLink(resource, entity.getStatus(), entity.getStage());
+            addTerminateLink(resource, entity.getStatus(), entity.getStage(), entity.getConfig().getService().getType());
             addCancelLink(resource, entity.getStatus());
             
             return resource;
@@ -97,12 +111,11 @@ public class JobResourceProcessor extends BaseResourceProcessor<Job> {
         @Override
         public Resource<DetailedJob> process(Resource<DetailedJob> resource) {
             DetailedJob entity = resource.getContent();
-
             addSelfLink(resource, entity);
             addGuiLink(resource, entity.getStatus(), entity.getGuiUrl());
             addLogsLink(resource);
             addOutputLinks(resource, entity.getOutputs());
-            addTerminateLink(resource, entity.getStatus(), entity.getStage());
+            addTerminateLink(resource, entity.getStatus(), entity.getStage(), entity.getConfig().getService().getType());
             addCancelLink(resource, entity.getStatus());
             
             return resource;
@@ -114,13 +127,11 @@ public class JobResourceProcessor extends BaseResourceProcessor<Job> {
         @Override
         public Resource<ShortJob> process(Resource<ShortJob> resource) {
             ShortJob entity = resource.getContent();
-
             addSelfLink(resource, entity);
             addGuiLink(resource, entity.getStatus(), entity.getGuiUrl());
             addLogsLink(resource);
-            addTerminateLink(resource, entity.getStatus(), entity.getStage());
+            addTerminateLink(resource, entity.getId());
             addCancelLink(resource, entity.getStatus());
-            
             return resource;
         }
     }
